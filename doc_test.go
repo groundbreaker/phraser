03b906478286f926dfb2910535dcc097187045eb
Eric Marden <eric.marden@gmail.com>
2018-12-06 11:49:48 -0600

    updates examples with how to override everything

--- doc_test.go
+++ doc_test.go
@@ -12,7 +12,23 @@ func Example() {
 	fmt.Println(phrase.Passphrase())
 }
 
-func ExampleParseWordList() {
-	phraser.ParseWordList("/path/to/words")
-	// Output: []string{"people", "humans","robots", "droids"}
+func Example_override() {
+	// Override the Default* vars before calling phraser.New()
+	// Use your own []string values, or use phraser.ParseWordList to laod it from a file.
+	phraser.DefaultSymbol = "#"
+	phraser.DefaultAdjectives = []string{"awesome"}
+	phraser.DefaultNouns = []string{"robots"}
+
+	p := phraser.New()
+	// You can also override values on the instance of phraser.
+	p.Adjectives = []string{"amiable"}
+
+	phrase := p.GeneratePhrase() // this sets the random values
+
+	// You can override the choosen values here as well.
+	phrase.Number = 42
+	phrase.Symbol = "?"
+
+	fmt.Println(phrase.Passphrase())
+	// Output: Amiable42Robots?
 }
